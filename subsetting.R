738c7d83fc5813c9aaf55a2f3651ecb83d924eb6
Annika Eriksson <annika.u.eriksson@gmail.com>
2014-07-29 22:21:20 -0700

    Fixed subsetting
    Also changed “solution” to “essentiality” to be more clear about what
    the values represent

--- subsetting.R
+++ subsetting.R
@@ -1,5 +1,5 @@
 # This script reads in the training data and subsets the names of genes
-# into those included in the solution file (Those for which we are supposed
+# into those included in the essentiality file (Those for which we are supposed
 # to predict essentiality scores - Achilles_v2.9_training.gct), those which
 # overlap with the expression and copy number data, and those for which all
 # data is available.
@@ -8,19 +8,19 @@
 
 expressfile <- "~/DREAM/CCLE_expression_training.gct"
 copynumfile <- "~/DREAM/CCLE_copynumber_training.gct"
-solutionfile <- "~/DREAM/Achilles_v2.9_training.gct"
+essentialfile <- "~/DREAM/Achilles_v2.9_training.gct"
 
 express <- read.delim(expressfile, skip=2, header=TRUE, stringsAsFactors=FALSE)
 copynum <- read.delim(copynumfile, skip=2, header=TRUE, stringsAsFactors=FALSE)
-solution <- read.delim(solutionfile, skip=2, header=TRUE, stringsAsFactors=FALSE)
+essentiality <- read.delim(essentialfile, skip=2, header=TRUE, stringsAsFactors=FALSE)
 
 ex.desc <- express$Description
 cn.desc <- copynum$Description
-# All the genes in the solution file
-so.desc <- solution$Description
-# Overlap between copynumber and solution
+# All the genes in the essentiality file
+so.desc <- essentiality$Description
+# Overlap between copynumber and essentiality
 cn.so.desc <- intersect(so.desc, cn.desc)
-# Overlap between expression and solution
+# Overlap between expression and essentiality
 ex.so.desc <- intersect(so.desc, ex.desc)
 # Genes in all 3
 all.desc <- intersect(ex.so.desc, cn.so.desc)
@@ -31,8 +31,14 @@ write.table(cn.so.desc, file="~/DREAM/copynum_genes.txt", sep="\t", quote=FALSE,
 write.table(ex.so.desc, file="~/DREAM/express_genes.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
 write.table(all.desc, file="~/DREAM/all_genes.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
 
-#Get all the values for these subsets
-genes_in_solution <- solution[which(intersect(solution$Description, so.desc)>0),]
-genes_in_cn.so <- solution[which(intersect(solution$Description, cn.so.desc)>0),]
-genes_in_ex.so <- solution[which(intersect(solution$Description, ex.so.desc)>0),]
-genes_in_all <- solution[which(intersect(solution$Description, all.desc)>0),]
+# Get all the values for these subsets
+essentiality.of.copynum <- essentiality[which(intersect(essentiality$Description, cn.so.desc)>0),]
+essentiality.of.express <- essentiality[which(intersect(essentiality$Description, ex.so.desc)>0),]
+essentiality.of.all <- essentiality[which(intersect(essentiality$Description, all.desc)>0),]
+
+# Futher subset values of expression and copy number arrays
+copynum.of.essentiality <- copynum[which(intersect(copynum$Description, cn.so.desc)>0),]
+copynum.of.all <- copynum[which(intersect(copynum$Description, all.desc)>0),]
+
+express.of.essentiality <- express[which(intersect(express$Description, ex.so.desc)>0),]
+express.of.all <- express[which(intersect(express$Description, all.desc)>0),]
